23faca61d88c18dd607abc34c5663b79dd41b51a
andrew4779 <andrewkimani02@gmail.com>
2023-01-04 08:51:55 +0300

    string concatenation

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,26 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 4) #addition(+)
+print(3 - 2) #subtraction(-)
+print(2 * 3) #multiplication(*)
+print(20 / 2) #division(/)
+print(3 ** 2) #exponential(**)
+print(10 % 2) #modules(%)
+print(40 // 10) #Floor division operators(//)
+
+# Checking data types
+
+print(type(10)) #  Int (Positive Int)
+print(type(-3)) # Int (Negative Int)
+print(type(3.14)) # Float
+print(type([1, 2, 3])) #List
+print(type({'name':'Andrew','age':'19'})) #Dictionary
+
+# dict = {}
+# dict ['name'] = 'Andrew'
+# dict ['age'] = '19'
+
+# print (dict)
+print(type({9.8, 3.14, 2.7})) #set
+print(type((9.8, 3.14, 2,7))) #Tuplep
---
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -0,0 +1,74 @@
+
+# Variables in Python
+
+first_name = 'Andrew'
+last_name = 'kimani'
+country = 'kenya'
+city = 'Nairobi'
+age = 250
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+    'firstname':'Andrew', 
+    'lastname':'kimani', 
+    'country':'kenya',
+    'city':'Nairobi'
+    }
+    
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+# first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+# print(first_name, last_name, country, age, is_married)
+# print('First name:', first_name)
+# print('Last name: ', last_name)
+# print('Country: ', country)
+# print('Age: ', age)
+# print('Married: ', is_married)
+
+# first_name = input('What is your name ?')
+# age = input('Your age')
+
+# print(first_name)
+# print(age)
+
+# print(first_name, age)
+
+# print(type('andrew'))
+# print(type(10))
+# print(type(False))
+# print(type(3.12))
+# print(type(['1+2']))
+# print(type({'andrew':'kimani'}))
+# print(type((1,2)))
+
+# string to List
+
+first_name = 'Andrew'
+print(first_name)
+first_name_to_list = list(first_name)
+print(first_name_to_list)
+
+# int to Float
+num_int = 10
+print(num_int)
+num_float = float(num_int)
+print('num_float:', num_float)
+
+# float to int
+
+gravity = 29.2
+print(int(gravity))
