d2dffa7bec513731da36fb5a5c34e5d525913421
andrew4779 <andrewkimani02@gmail.com>
2023-01-18 13:14:32 +0300

    Add dict file

---
+++ 08_Day_Dictionaries/08_Day.py
@@ -0,0 +1,15 @@
+person = { 
+    'first_name' : 'Andrew',
+    'second_name' : 'Kimani',
+    'age' : '20',
+    'country' : 'Kenya',
+    'is_married' : 'False',
+    'skills' : ['Javascript', 'Python', 'React', 'Angular', 'Html', 'CSS'],
+    'address' : {
+        'street': 'space',
+        'zipcode': '41'
+    }
+
+ }
+
+print(person)
