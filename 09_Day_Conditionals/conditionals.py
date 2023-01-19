ba674db097ee9c722989c73d3cd0523aa0b7c93b
andrew4779 <andrewkimani02@gmail.com>
2023-01-19 11:18:30 +0300

    conditionals

---
+++ 09_Day_Conditionals/conditionals.py
@@ -0,0 +1,41 @@
+a = 3
+if a > 0:
+    print('A is a positive number')
+
+
+# ||
+
+b = 9
+if b < 19:
+    print('B is a  negative number')
+
+
+# //
+
+c = 7
+if c > 12:
+    print('C is a positive number')
+
+else:
+    print('C is a negative number')
+
+
+# //
+
+d = 9
+if d > 9:
+    print('d is greater')
+
+elif d < 8:
+    print('d is lesser')
+else:
+    print('d is zero')
+
+
+user = 'Andrew'
+access_level = 3
+if user =='admin' or access_level < 1:
+    print("Access granted!!")
+
+else:
+    print("Access denied!!")
