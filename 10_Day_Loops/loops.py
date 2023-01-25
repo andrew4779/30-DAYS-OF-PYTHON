6d1d7f45e90c21a3a32aecb988153e09c87be3c1
andrew4779 <andrewkimani02@gmail.com>
2023-01-25 12:15:16 +0300

    loops

--- 10_Day_Loops/loops.py
+++ 10_Day_Loops/loops.py
@@ -1,27 +1,29 @@
-# count = 0
-# while count < 5:
-#     print(count)
-#     count = count + 1
-# else:
-#     print(count)
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+else:
+    print(count)
 
 
 
 
-# it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
-# for company in it_companies:
-#     print(company)
-# # 
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+for company in it_companies:
+    print(company)
+# 
 
 
-# numbers = [0, 1, 2, 3, 4, 5]
-# for number in numbers:
-#     print(numbers)
+numbers = [0, 1, 2, 3, 4, 5]
+for number in numbers:
+    print(numbers)
 
 
-# language = 'python'
-# for letter in language:
-#     print(language)
+language = 'python'
+for letter in language:
+    print(language)
 
-for i in range(len(language)):
-    print(language(i))
+numbers = [0, 1, 2, 3, 4, 5]
+for number in numbers:
+    print(numbers)
+
