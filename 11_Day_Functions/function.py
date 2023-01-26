19275bd34bc7510ea401a304fa6b5a7980a2e8c2
andrew4779 <andrewkimani02@gmail.com>
2023-01-26 11:12:34 +0300

    functions

--- 11_Day_Functions/function.py
+++ 11_Day_Functions/function.py
@@ -1,5 +1,16 @@
-# Declaring a function
-def function_name():
-    codes
-    codes
-function_name()
+# # Declaring a function
+# def function_name():
+#     codes
+#     codes
+# function_name()
+
+
+
+def generate_full_name():
+    first_name = 'Andrew'
+    last_name = 'kimani'
+    space = ' '
+    full_name = first_name + space + last_name
+    return full_name
+
+print(generate_full_name())
