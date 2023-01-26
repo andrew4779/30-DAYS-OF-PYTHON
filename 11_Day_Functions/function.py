d216682fed3860aeb7a862ef5741c17b027de3c1
andrew4779 <andrewkimani02@gmail.com>
2023-01-26 11:34:56 +0300

    functions

--- 11_Day_Functions/function.py
+++ 11_Day_Functions/function.py
@@ -6,11 +6,19 @@
 
 
 
-def generate_full_name():
-    first_name = 'Andrew'
-    last_name = 'kimani'
-    space = ' '
-    full_name = first_name + space + last_name
-    return full_name
+# def generate_full_name():
+#     first_name = 'Andrew'
+#     last_name = 'kimani'
+#     space = ' '
+#     full_name = first_name + space + last_name
+#     return full_name
 
-print(generate_full_name())
+# print(generate_full_name())
+
+
+
+def greetings (name):
+    message = name + ', welcome to Python for Everyone!'
+    return message
+
+print(greetings('Andrew'))
