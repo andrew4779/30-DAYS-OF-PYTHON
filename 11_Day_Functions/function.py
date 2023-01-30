353eb439b4e87a426d2d39f55687285df54dc898
andrew4779 <andrewkimani02@gmail.com>
2023-01-30 12:35:47 +0300

    functions

--- 11_Day_Functions/function.py
+++ 11_Day_Functions/function.py
@@ -38,3 +38,7 @@ def calculate_age (current_year, birth_year):
 
 print('Age:', calculate_age(2023, 2002))
 
+def add_two_numbers (num1, num2):
+    total = num1 + num2
+    return total
+print(add_two_numbers(10, 15))
