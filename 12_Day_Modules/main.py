59388d54bd79c18338ff23cb112a041e71960189
andrew4779 <andrewkimani02@gmail.com>
2023-01-30 21:19:55 +0300

    imports

--- 12_Day_Modules/main.py
+++ 12_Day_Modules/main.py
@@ -1,9 +1,9 @@
-# from statistics import *
-# ages = [20, 23, 21, 25, 27 ,29, 28, 22]
+from statistics import *
+ages = [20, 23, 21, 25, 27 ,29, 28, 22]
 
-# print(mean(ages))
-# print(median(ages))
-# print(mode(ages))
+print(mean(ages))
+print(median(ages))
+print(mode(ages))
 
 import os
 
