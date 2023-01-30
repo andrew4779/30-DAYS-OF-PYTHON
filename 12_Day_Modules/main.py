c65f79b2c7daf5ed8eb1088717d5b68853d77fc6
andrew4779 <andrewkimani02@gmail.com>
2023-01-30 21:19:36 +0300

    main.py

--- 12_Day_Modules/main.py
+++ 12_Day_Modules/main.py
@@ -1,6 +1,11 @@
-from statistics import *
-ages = [20, 23, 21, 25, 27 ,29, 28, 22]
+# from statistics import *
+# ages = [20, 23, 21, 25, 27 ,29, 28, 22]
 
-print(mean(ages))
-print(median(ages))
-print(mode(ages))
+# print(mean(ages))
+# print(median(ages))
+# print(mode(ages))
+
+import os
+
+os.mkdir('kimani')
+os.rmdir()
