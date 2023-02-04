52da828f58ee80064def97246e8516fb1c37154a
andrew4779 <andrewkimani02@gmail.com>
2023-02-04 14:33:41 +0300

    lists comprehension

--- 13_Day_List_comprehension/list.py
+++ 13_Day_List_comprehension/list.py
@@ -5,3 +5,14 @@ print(lst)
 
 numbers = [i for i in range(11)]
 print(numbers)
+
+
+
+numbers = [i for i in range (22)]
+print(numbers)
+
+squares = [i * i for i in range(11)]
+print(squares)
+
+numbers = [(i, i * i) for i in range(11)]
+print(numbers)
