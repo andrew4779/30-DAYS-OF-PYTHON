976c6ab6aa48bef8020a0dcc1d3750a45db01f47
andrew4779 <andrewkimani02@gmail.com>
2023-02-08 08:16:26 +0300

    absolute function

--- 14_Day_Higher_order_functions/higher_function.py
+++ 14_Day_Higher_order_functions/higher_function.py
@@ -8,3 +8,15 @@ def higher_order_function(f, lst):
 
 result = higher_order_function(sum_numbers, [1, 2, 3, 4, 5])
 print(result)
+
+def square(x):
+    return(x ** 2)
+
+def cube(x):
+    return(x ** 3)
+
+def absolute(x):
+    if x >= 0:
+        return x
+    else:
+        return -(x)
