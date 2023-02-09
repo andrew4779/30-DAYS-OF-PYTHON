26e3a834ac93d301f750160fa0fc0992c068c1f2
andrew4779 <andrewkimani02@gmail.com>
2023-02-09 08:39:02 +0300

    datetime info

--- 16_Day_Python_date_time/pythondt.py
+++ 16_Day_Python_date_time/pythondt.py
@@ -1,2 +1,9 @@
-import datetime
-print(dir(datetime))
+# import datetime
+# print(dir(datetime))
+
+from datetime import datetime
+now = datetime.now()
+print(now)
+date = now.day
+month = now.month
+year = now.year
