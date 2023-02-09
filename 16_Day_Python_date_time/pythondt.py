dd3b17d359a1dbf37db217a14b5573ce6f453ab6
andrew4779 <andrewkimani02@gmail.com>
2023-02-09 09:01:41 +0300

    datetime

--- 16_Day_Python_date_time/pythondt.py
+++ 16_Day_Python_date_time/pythondt.py
@@ -15,3 +15,15 @@ print(new_year)
 
 date_string = "9 Feb, 2023"
 print("date_string=", date_string)
+
+
+from datetime import time
+
+a = time()
+print("a = ", a)
+
+b = time (10, 30, 50)
+print("b=", b)
+
+c = time(hour=10, minute=40, second=20)
+print("c=", c)
