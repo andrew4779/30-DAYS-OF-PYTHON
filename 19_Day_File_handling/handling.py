78ef8df9478fab63dd362f7773524d34377e70ed
andrew4779 <andrewkimani02@gmail.com>
2023-02-15 10:57:56 +0300

    string form

--- 19_Day_File_handling/handling.py
+++ 19_Day_File_handling/handling.py
@@ -6,8 +6,18 @@
 # # print(f)
 # f.close()
 
-f = open('./files/reading_file_example.txt')
-lines = f.readlines()
-print(type(lines))
-print(lines)
-f.close()
+# f = open('./files/reading_file_example.txt')
+# lines = f.readlines()
+# print(type(lines))
+# print(lines)
+# f.close()
+
+person_dct = {
+    "name": "Andrew",
+    "country":"Kenya",
+    "city":"Nairobi",
+    "skills": ["Javascript", "Python", "Angular"]
+
+}
+
+person_json = "{'name': 'Asabeneh', 'country': 'Finland', 'city': 'Helsinki', 'skills': ['JavaScrip', 'React', 'Python']}"
