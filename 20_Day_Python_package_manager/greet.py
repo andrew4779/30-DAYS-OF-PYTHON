dc2d80be28752900b406d18cd8b52b48d7818137
andrew4779 <andrewkimani02@gmail.com>
2023-02-15 19:11:46 +0300

    python packages

--- 20_Day_Python_package_manager/greet.py
+++ 20_Day_Python_package_manager/greet.py
@@ -18,12 +18,20 @@
 # To open a network connection you need a package  called _requests_
 
 
+# import requests
+
+# url = 'https://www.w3.org/TR/PNG/iso_8859-1.txt'
+
+# response = requests.get(url)
+# print(response)
+# print(response.status_code)
+# print(response.headers)
+# print(response.text)
+
 import requests
-
-url = 'https://www.w3.org/TR/PNG/iso_8859-1.txt'
-
-response = requests.get(url)
-print(response)
-print(response.status_code)
-print(response.headers)
-print(response.text)
+url = 'https://restcountries.eu/rest/v2/all'  # countries api
+response = requests.get(url)  # opening a network and fetching a data
+print(response) # response object
+print(response.status_code)  # status code, success:200
+countries = response.json()
+print(countries[:1])
