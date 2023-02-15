f6f6e87de135275d0cf7cc9a788ca57f4208a947
andrew4779 <andrewkimani02@gmail.com>
2023-02-15 11:15:12 +0300

    json to dict

--- 19_Day_File_handling/handling.py
+++ 19_Day_File_handling/handling.py
@@ -12,6 +12,8 @@
 # print(lines)
 # f.close()
 
+
+
 person_dct = {
     "name": "Andrew",
     "country":"Kenya",
@@ -21,3 +23,19 @@ person_dct = {
 }
 
 person_json = "{'name': 'Asabeneh', 'country': 'Finland', 'city': 'Helsinki', 'skills': ['JavaScrip', 'React', 'Python']}"
+
+
+import json
+# JSON
+person_json = '''{
+    "name": "Andrew",
+    "country": "Kenya",
+    "city": "Nairobi",
+    "skills": ["JavaScrip", "React", "Python"]
+}'''
+# let's change JSON to dictionary
+person_dct = json.loads(person_json)
+print(type(person_dct))
+print(person_dct)
+print(person_dct['name'])
+print(person_dct['city'])
