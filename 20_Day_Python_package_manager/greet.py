f726f5a9e74998545695ec279be426c7db07d6ed
andrew4779 <andrewkimani02@gmail.com>
2023-02-15 19:05:08 +0300

    python packages

--- 20_Day_Python_package_manager/greet.py
+++ 20_Day_Python_package_manager/greet.py
@@ -2,17 +2,28 @@
 #     return f'{firstname} {lastname}, welcome to 30DaysOfPython Challenge!'
 
 
-import webbrowser
+# import webbrowser
 
-url_lists = [
-    'http://www.python.org',
-    'https://www.linkedin.com/in/drew-kimani/',
-    'https://github.com/andrew4779',
-    'https://twitter.com/masaneii',
-]
+# url_lists = [
+#     'http://www.python.org',
+#     'https://www.linkedin.com/in/drew-kimani/',
+#     'https://github.com/andrew4779',
+#     'https://twitter.com/masaneii',
+# ]
 
-for url in url_lists:
-    webbrowser.open_new_tab(url)
+# for url in url_lists:
+#     webbrowser.open_new_tab(url)
 
 # API- Application program interface
 # To open a network connection you need a package  called _requests_
+
+
+import requests
+
+url = 'https://www.w3.org/TR/PNG/iso_8859-1.txt'
+
+response = requests.get(url)
+print(response)
+print(response.status_code)
+print(response.headers)
+print(response.text)
