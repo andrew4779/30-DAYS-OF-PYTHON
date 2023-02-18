24b7dfd946ac0e4c633a0537cd4312be02194520
andrew4779 <andrewkimani02@gmail.com>
2023-02-18 12:43:02 +0300

    scraping data

--- 22_Day_Web_scraping/scrappeddata.py
+++ 22_Day_Web_scraping/scrappeddata.py
@@ -5,4 +5,18 @@ url = 'https://andrew4779.github.io/Plio/'
 
 response = requests.get(url)
 status = response.status_code
-print(status)
+print(status) 
+
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://moringaschool.com/"
+
+response = requests.get(url)
+content = response.content
+soup = BeautifulSoup(content, 'html.parser')
+print(soup.title)
+print(soup.title.get_text())
+print(soup.body)
+print(response.status_code)
