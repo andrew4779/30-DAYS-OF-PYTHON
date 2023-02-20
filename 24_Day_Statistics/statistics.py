af33423190d4ff4d03beeeb7ae5f6c2bff460843
andrew4779 <andrewkimani02@gmail.com>
2023-02-20 08:44:06 +0300

    checking data types

--- 24_Day_Statistics/statistics.py
+++ 24_Day_Statistics/statistics.py
@@ -10,3 +10,9 @@ numpy_array_from_list = np.array([1, 2, 3, 4, 5])
 print('original array:' , numpy_array_from_list)
 ten_times_original = numpy_array_from_list ** 2
 print(ten_times_original)
+
+numpy_int_arr = np.array([1, 2, 3, 4, 5])
+numpy_float_arr = np.array([1.1, 2.0, 3.2])
+
+print(numpy_int_arr.dtype)
+print(numpy_float_arr.dtype)
