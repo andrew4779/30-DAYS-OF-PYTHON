e86f1fab0cb81ecc66d8a7d49ee3ad3e1ae21198
andrew4779 <andrewkimani02@gmail.com>
2023-02-20 08:36:57 +0300

    floor division

--- 24_Day_Statistics/statistics.py
+++ 24_Day_Statistics/statistics.py
@@ -2,4 +2,6 @@ import numpy as np
 
 
 numpy_array_from_list = np.array([1, 2, 3, 4, 5])
-print('original array:')
+print('original array:' , numpy_array_from_list)
+ten_times_original = numpy_array_from_list // 10
+print(ten_times_original)
