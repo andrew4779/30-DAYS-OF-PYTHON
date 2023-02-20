144a48c3c86cd2b5c028bd9b1581244dfe705d42
andrew4779 <andrewkimani02@gmail.com>
2023-02-20 08:54:52 +0300

    multi-dimensional array

--- 24_Day_Statistics/statistics.py
+++ 24_Day_Statistics/statistics.py
@@ -16,3 +16,6 @@ numpy_float_arr = np.array([1.1, 2.0, 3.2])
 
 print(numpy_int_arr.dtype)
 print(numpy_float_arr.dtype)
+
+two_dimension_array = np.array([(1, 2, 3), (4, 5, 6), (7, 8, 9)])
+print(type (two_dimension_array))
