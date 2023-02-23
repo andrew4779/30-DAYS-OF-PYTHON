30cad0388210b55a30d9cc73daf1159570ef68bb
andrew4779 <andrewkimani02@gmail.com>
2023-02-24 01:06:01 +0300

    python templates

--- python_for_web/app.py
+++ python_for_web/app.py
@@ -37,3 +37,5 @@ if __name__ == '__main__':
     # to make it work for both production and development
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
+    
