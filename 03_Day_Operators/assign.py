591a6a75cc698a6805b6961b444686966702303c
Andrew kimani <91945077+andrew4779@users.noreply.github.com>
2023-03-07 06:34:48 +0000

    declare values

--- 03_Day_Operators/assign.py
+++ 03_Day_Operators/assign.py
@@ -8,3 +8,12 @@ diff = b - a
 
 print('a + b = ', total)
 print('b - a = ', diff)
+
+num_one = 3
+num_two = 4
+
+total = num_one + num_two
+diff = num_two - num_one
+
+print('total: ', total)
+print('difference: ', diff)
